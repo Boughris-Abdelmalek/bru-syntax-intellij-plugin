a372d34c0d509cbb33f8a5ed4e958ae149d5c7da
GitHub Action <action@github.com>
2025-02-22 10:22:24 +0000

    Template cleanup

--- src/main/kotlin/com/github/boughrisabdelmalek/brusyntaxintellijplugin/MyBundle.kt
+++ src/main/kotlin/com/github/boughrisabdelmalek/brusyntaxintellijplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.boughrisabdelmalek.brusyntaxintellijplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/boughrisabdelmalek/brusyntaxintellijplugin/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/boughrisabdelmalek/brusyntaxintellijplugin/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.boughrisabdelmalek.brusyntaxintellijplugin.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.boughrisabdelmalek.brusyntaxintellijplugin.MyBundle
+import com.github.boughrisabdelmalek.brusyntaxintellijplugin.services.MyProjectService
 import javax.swing.JButton
 
 
